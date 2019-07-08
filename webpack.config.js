3b7b2644a9619fb8695638dcf874e165aea4c000
wanmaoor <wanmao1284879375@outlook.com>
2019-07-08 16:33:25 +0800

    add ts-config & webpack-config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+module.exports = {
+	mode: "development",
+	entry: {
+		index: './lib/index.tsx'
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist/lib'),
+		library: 'LR',
+		libraryTarget: "umd"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'awesome-typescript-loader'
+			}
+		]
+	}
+}
