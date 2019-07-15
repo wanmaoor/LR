05fba107ba40c0cea2fd6909eb14bebf757f5145
wanmaoor <wanmao1284879375@outlook.com>
2019-07-15 11:49:28 +0800

    move coverage into jest.config

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,9 @@ module.exports = {
 	verbose: true,
 	clearMocks: false,
 	collectCoverage: true,
-	// collectCoverageFrom: ["{lib,include}/**/*.{js,jsx,ts,tsx}", "!**/node_modules/**"],
-	// coverageDirectory: 'coverage',
-	// coverageReporters: ['text', 'lcov'],
+	collectCoverageFrom: ["{lib,include}/**/*.{js,jsx,ts,tsx}", "!**/node_modules/**"],
+	coverageDirectory: 'coverage',
+	coverageReporters: ['text', 'lcov'],
 	reporters: ['default', 'jest-junit'],
 	// globals: {
 	// 	'ts-jest': {
